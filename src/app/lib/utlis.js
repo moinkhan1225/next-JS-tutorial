e7df42a9337f5f2abf0496f4ffa369cb49ded442
Hakai <moinkhanrobin1@gmail.com>
2024-02-06 03:51:09 +0530

    Added Fix to Undefined ReadyState

--- src/app/lib/utlis.js
+++ src/app/lib/utlis.js
@@ -1,18 +1,46 @@
+// import mongoose from "mongoose";
+
+// const connection ={};
+
+// export const connectDB = async () =>{
+//     try{
+//         if(connection.isConnected){
+//             console.log("using Existing Connection")
+//             return;
+//         }
+//         const db = await mongoose.connect(process.env.MONGO);
+//         connection.isConnected = db.connection[0].readyState;
+//     }
+//     catch(error){
+//         console.log(error);
+//         throw new Error(error)
+//     }
+// }
+
 import mongoose from "mongoose";
 
-const connection ={};
+const connection = {};
 
-export const connectDB = async () =>{
-    try{
-        if(connection.isConnected){
-            console.log("using Existing Connection")
+export const connectDB = async () => {
+    try {
+        if (connection.isConnected) {
+            console.log("Using Existing Connection");
             return;
         }
-        const db = await mongoose.connect(process.env.MONGO);
-        connection.isConnected = db.connection[0].readyState;
+
+        const db = await mongoose.connect(process.env.MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        });
+
+        connection.isConnected = db.connection.readyState;
+
+        console.log("New Connection Established");
+
+    } catch (error) {
+        console.error(error);
+        throw new Error(error);
     }
-    catch(error){
-        console.log(error);
-        throw new Error(error)
-    }
-}
+};
